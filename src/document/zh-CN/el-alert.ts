53f81babaee01a3d37ccc1026aead2d00c11c2cc
AloneH <admin@oibit.cn>
2021-04-15 17:18:41 +0800

    docs: el-alert for zh-cn

---
+++ src/document/zh-CN/el-alert.ts
@@ -0,0 +1,26 @@
+import { DocumentAttribute } from "typings/attribute";
+import { ElDocument } from "typings/document";
+import { DocumentEvent } from "typings/event";
+import { DocumentSlot } from "typings/slot";
+
+export const attributes: DocumentAttribute[] = [
+  { name: "title", description: "标题", type: "string", value: "—", default: "—" },
+  { name: "type", description: "主题", type: "string", value: "success/warning/info/error", default: "info" },
+  { name: "description", description: "辅助性文字。也可通过默认 slot 传入", type: "string", value: "—", default: "—" },
+  { name: "closable", description: "是否可关闭", type: "boolean", value: "—", default: true },
+  { name: "center", description: "文字是否居中", type: "boolean", value: "—", default: true },
+  { name: "close-text", description: "关闭按钮自定义文本", type: "string", value: "—", default: "—" },
+  { name: "show-icon", description: "是否显示图标", type: "boolean", value: "—", default: false },
+  { name: "effect", description: "选择提供的主题", type: "string", value: "light/dark", default: "light" },
+];
+
+export const slots: DocumentSlot[] = [
+  { name: "—", description: "描述" },
+  { name: "title", description: "标题的内容" },
+];
+
+export const methods: DocumentEvent[] = [{ name: "close", description: "关闭alert时触发的事件", parameter: "—" }];
+
+export const document: ElDocument = { attributes, slots, methods };
+
+export default document;
