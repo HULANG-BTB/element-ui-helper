4f61e48693c93862fb7a6027484f8e0faa0d5962
AloneH <admin@oibit.cn>
2021-04-16 21:45:57 +0800

    style: style lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: "module",
+    sourceType: 'module'
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "@typescript-eslint/naming-convention": "warn",
-    "@typescript-eslint/semi": "warn",
-    curly: "warn",
-    eqeqeq: "warn",
-    "no-throw-literal": "warn",
-    semi: "off",
+    '@typescript-eslint/naming-convention': 'warn',
+    '@typescript-eslint/semi': 'off',
+    curly: 'warn',
+    eqeqeq: 'warn',
+    'no-throw-literal': 'warn',
+    semi: 'off'
   },
-  ignorePatterns: ["**/*.d.ts"],
-};
+  ignorePatterns: ['**/*.d.ts']
+}
