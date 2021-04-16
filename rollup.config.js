9af12c4fafdc914a7bdff08d849dd2034b4f45c6
AloneH <admin@oibit.cn>
2021-04-16 17:33:19 +0800

    chore: del dist dir when run build

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,28 @@
 /* eslint-disable */
+import fs from "fs";
 import path from "path";
 import pkg from "./package.json";
 import { terser } from "rollup-plugin-terser";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
+
+function delDir(path) {
+  let files = [];
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach((file, index) => {
+      let curPath = path + "/" + file;
+      if (fs.statSync(curPath).isDirectory()) {
+        delDir(curPath); //递归删除文件夹
+      } else {
+        fs.unlinkSync(curPath); //删除文件
+      }
+    });
+    fs.rmdirSync(path);
+  }
+}
+
+delDir(path.resolve(__dirname, "dist"));
 
 const deps = Object.keys(pkg.dependencies || {});
 
@@ -21,6 +40,7 @@ export default [
       typescript({
         tsconfigOverride: {
           compilerOptions: {
+            module: "ESNext",
             declaration: false,
           },
           exclude: ["node_modules"],
